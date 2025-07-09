6b162479c5a9fb1ed58385cab845a25d774d1ac3
mikecarter216 <michaelakpan216@gmail.com>
2025-07-09 21:13:34 +0100

    fix

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const invController = require("../controllers/invcontroller")
+const invController = require("../controllers/invController")
 
 router.get("/type/:classification", invController.buildByClassification)
 router.get("/detail/:invId", invController.buildDetailView)
